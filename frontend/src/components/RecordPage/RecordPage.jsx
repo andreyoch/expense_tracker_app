bee3d8a8cc968d56ee3aacf1630c593856a0a658
Andrzej Ciszewski <at100991@student.sgh.waw.pl>
2021-04-11 08:44:34 +0200

    Transfer record page class to functional component

--- frontend/src/components/RecordPage/RecordPage.jsx
+++ frontend/src/components/RecordPage/RecordPage.jsx
@@ -1,51 +1,51 @@
-import React, {Component} from 'react';
+import React, {Component,useEffect} from 'react';
 import RecordService from "../../services/RecordService";
 
-class RecordPage extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                records:[]
-            }
-        }
-        componentDidMount() {
-            RecordService.getRecords().then((response) =>
-                this.setState({records: response.data}))
-        }
 
-    render() {
-        return (
-            <div>
-                <h2 className='text-center'>Employee list</h2>
-                <div className='row'>
-                    <table className='table table-striped table-bordered'>
-                        <thead>
-                        <tr>
-                            <th>Record type</th>
-                            <th>Amount</th>
-                            <th>Category</th>
-                            <th>Date</th>
-                            <th>Commentary</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.records.map((r) =>
-                                <tr key={r.id}>
-                                    <td>{r.amount}</td>
-                                    <td>{r.record_type}</td>
-                                    <td>{r.category}</td>
-                                    <td>{r.date}</td>
-                                    <td>{r.commentary}</td>
-                                </tr>
-                            )
-                        }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        );
-    }
-}
+ const RecordPage = () => {
+     const [records,setRecords] = React.useState([]);
+
+     useEffect(() => {
+         RecordService.getRecords().then((response) => {
+             setRecords(response.data);
+         })
+     })
+
+
+             return (
+                 <div>
+                     <h2 className='text-center'>Employee list</h2>
+                     <div className='row'>
+                         <table className='table table-striped table-bordered'>
+                             <thead>
+                             <tr>
+                                 <th>Record type</th>
+                                 <th>Amount</th>
+                                 <th>Category</th>
+                                 <th>Date</th>
+                                 <th>Commentary</th>
+                             </tr>
+                             </thead>
+                             <tbody>
+                             {
+                                 records.map((r) =>
+                                     <tr key={r.id}>
+                                         <td>{r.amount}</td>
+                                         <td>{r.record_type}</td>
+                                         <td>{r.category}</td>
+                                         <td>{r.date}</td>
+                                         <td>{r.commentary}</td>
+                                     </tr>
+                                 )
+                             }
+                             </tbody>
+                         </table>
+                     </div>
+                 </div>
+             );
+
+ }
+
+
 
 export default RecordPage;
