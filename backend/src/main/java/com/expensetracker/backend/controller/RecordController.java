5076ba56378d0a504dbdc6f052e9eed9677099cd
Andrzej Ciszewski <at100991@student.sgh.waw.pl>
2021-04-12 16:27:52 +0200

    Do update record rest api

--- backend/src/main/java/com/expensetracker/backend/controller/RecordController.java
+++ backend/src/main/java/com/expensetracker/backend/controller/RecordController.java
@@ -3,13 +3,17 @@ package com.expensetracker.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.expensetracker.backend.exception.ResourceNotFoundException;
 import com.expensetracker.backend.model.Record;
 import com.expensetracker.backend.repository.RecordRepository;
 @CrossOrigin(origins="http://localhost:3000")
@@ -28,7 +32,24 @@ public class RecordController {
 	
 	// create employee rest api
 		@PostMapping("/records")
-		public Record createEmployee(@RequestBody Record record) {
+		public Record createRecord(@RequestBody Record record) {
 			return recordRepository.save(record);
 }
+		//Update record info by id
+		@PutMapping("/records/{id}")
+		public ResponseEntity<Record> updateRecord(@PathVariable Long id, @RequestBody Record recordDetails){
+			Record record = recordRepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Record with id:" + id + "not exist"));
+			
+			record.setAmount(recordDetails.getAmount());
+			record.setCategory(recordDetails.getCategory());
+			record.setCommentary(recordDetails.getCommentary());
+			record.setDate(recordDetails.getDate());
+			record.setRecordType(recordDetails.getRecordType());
+			
+			
+			Record updatedRecord = recordRepository.save(record);
+			return ResponseEntity.ok(updatedRecord);
+			
+		}
 }
