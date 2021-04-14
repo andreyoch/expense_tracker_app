c55d79bf7581cfab2b0b14480109adbc603d6ed4
Andrzej Ciszewski <at100991@student.sgh.waw.pl>
2021-04-14 07:45:13 +0200

    Add account info status

--- frontend/src/components/RecordPage/RecordPage.jsx
+++ frontend/src/components/RecordPage/RecordPage.jsx
@@ -1,82 +1,103 @@
-import React, {Component,useEffect} from 'react';
+import React, {Component, useEffect} from 'react';
 import RecordService from "../../services/RecordService";
 import AddRecordModal from "./AddRecordModal/AddRecordModal";
 import s from './RecordPage.module.css'
 import EditRecordModal from "./EditRecordModal/EditRecordModal";
 
 
- const RecordPage = () => {
-     const [records,setRecords] = React.useState([]);
-     const[isAddRecordModalShow,setAddRecordModalShow] = React.useState(false);
-     const [isEditRecordModalShow,setEditRecordModalShow] = React.useState(false)
-     const [recordToEdit,setRecordToEdit] = React.useState({});
-      useEffect(() => {
-             RecordService.getRecords().then((response) => {
-                 setRecords(response.data);
-             })
-     },[])
-           const  closeAddRecordModal = () => {
-         setAddRecordModalShow(false)
-           }
+const RecordPage = () => {
+    const [records, setRecords] = React.useState([]);
+    const [isAddRecordModalShow, setAddRecordModalShow] = React.useState(false);
+    const [isEditRecordModalShow, setEditRecordModalShow] = React.useState(false)
+    const [recordToEdit, setRecordToEdit] = React.useState({});
+    useEffect(() => {
+        RecordService.getRecords().then((response) => {
+            setRecords(response.data);
+        })
+    }, [])
+    const closeAddRecordModal = () => {
+        setAddRecordModalShow(false)
+    }
 
-           const updateRecordsList = () => {
-               RecordService.getRecords().then((response) => {
-                   setRecords(response.data);
-               })
-           }
-           const deleteRecord = (e,id) => {
-         const recordHtmlElement = e.target.parentElement;
-                RecordService.deleteRecord(id).then( () => recordHtmlElement.remove());
-           }
+    const updateRecordsList = () => {
+        RecordService.getRecords().then((response) => {
+            setRecords(response.data);
+        })
+    }
+    const deleteRecord = (e, id) => {
+        const recordHtmlElement = e.target.parentElement;
+        RecordService.deleteRecord(id).then(() => recordHtmlElement.remove());
+    }
 
-           const editRecordInfo = (record) => {
-                setRecordToEdit(record);
-                setEditRecordModalShow(true);
-           }
+    const editRecordInfo = (record) => {
+        setRecordToEdit(record);
+        setEditRecordModalShow(true);
+    }
 
-           const closeEditRecordModal = () => {
-          setEditRecordModalShow(false);
-           }
+    const closeEditRecordModal = () => {
+        setEditRecordModalShow(false);
+    }
+    const getAccountStatus = () => {
+        let accountSum = 0;
+        records.map((r) => {
+            if(r.recordType === 'Income') {
+                accountSum += r.amount;
+            } else if (r.recordType === 'Expense') {
+                accountSum -= r.amount;
+            }
+        })
+        return accountSum;
+    }
+        const positive = {
+        color: "green",
+        }
+        const negative = {
+        color: 'red'
+        }
+        const accountStateStyles = getAccountStatus() > 0 ? positive : negative;
+    return (
+        <div>
+            <h2 className='text-center'>Records</h2>
+            <div>State of an account</div>
+            <div style={accountStateStyles}>{getAccountStatus()}</div>
+            <button onClick={() => setAddRecordModalShow(true)}> Show modal</button>
+            <AddRecordModal show={isAddRecordModalShow} closeModal={closeAddRecordModal}
+                            updateRecordsList={updateRecordsList}/>
+            {isEditRecordModalShow ? <EditRecordModal isShow={isEditRecordModalShow} record={recordToEdit}
+                                                      closeEditRecordModal={closeEditRecordModal}
+                                                      updateRecordsList={updateRecordsList}/> : ""}
+            <div className='row'>
+                <table className='table table-striped table-bordered'>
+                    <thead>
+                    <tr>
+                        <th>Record type</th>
+                        <th>Amount</th>
+                        <th>Category</th>
+                        <th>Date</th>
+                        <th>Commentary</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        records.map((r) =>
+                            <tr key={r.id}>
+                                <td>{r.recordType}</td>
+                                <td>{r.amount}</td>
+                                <td>{r.category}</td>
+                                <td>{r.date}</td>
+                                <td>{r.commentary}</td>
+                                <td className={s.close} onClick={(e) => deleteRecord(e, r.id)}>&times;</td>
+                                <td className={s.edit} onClick={() => editRecordInfo(r)}>✏</td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 
-             return (
-                 <div>
-                     <h2 className='text-center'>Records</h2>
-                    <button onClick={() => setAddRecordModalShow(true)}> Show modal</button>
-                     <AddRecordModal show={isAddRecordModalShow} closeModal={closeAddRecordModal} updateRecordsList={updateRecordsList}/>
-                     {isEditRecordModalShow ? <EditRecordModal  isShow={isEditRecordModalShow} record={recordToEdit} closeEditRecordModal={closeEditRecordModal} updateRecordsList={updateRecordsList} /> : ""}
-                     <div className='row'>
-                         <table className='table table-striped table-bordered'>
-                             <thead>
-                             <tr>
-                                 <th>Record type</th>
-                                 <th>Amount</th>
-                                 <th>Category</th>
-                                 <th>Date</th>
-                                 <th>Commentary</th>
-                             </tr>
-                             </thead>
-                             <tbody>
-                             {
-                                 records.map((r) =>
-                                     <tr key={r.id}>
-                                         <td>{r.recordType}</td>
-                                         <td>{r.amount}</td>
-                                         <td>{r.category}</td>
-                                         <td>{r.date}</td>
-                                         <td>{r.commentary}</td>
-                                         <td className={s.close} onClick={(e)=> deleteRecord(e,r.id)}>&times;</td>
-                                         <td className={s.edit} onClick={() =>editRecordInfo(r)}>✏</td>
-                                     </tr>
-                                 )
-                             }
-                             </tbody>
-                         </table>
-                     </div>
-                 </div>
-             );
-
- }
-
+}
 
 
 export default RecordPage;
