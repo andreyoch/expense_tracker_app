aff40d6c4d307a31fabaa8279d0ccffea1ba55f2
Andrzej Ciszewski <at100991@student.sgh.waw.pl>
2021-04-17 09:17:32 +0200

    Connect store to components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import RecordPage from "./components/RecordPage/RecordPage";
 import {BrowserRouter, NavLink, Route} from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
 import Header from "./components/Header/Header";
+import RecordPageContainer from "./components/RecordPage/RecordPageContainer";
 
 function App() {
   return (
 
       <BrowserRouter>
           <Header/>
-      <Route path={"/records"} render={() => <RecordPage />}/>
+      <Route path={"/records"} render={() => <RecordPageContainer />}/>
       <Route path={'/main'} render={() => <MainPage />}/>
       </BrowserRouter>
   );
